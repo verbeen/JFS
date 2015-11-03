a9edcdc59c3cf2be2427d721599d5bf187762507
verbeen <l_puddu@hotmail.com>
2015-11-03 12:05:17 +0100

    changed id to email as login id

--- JFSService/src/main/java/jfs/service/transferobjects/LoginDTO.java
+++ JFSService/src/main/java/jfs/service/transferobjects/LoginDTO.java
@@ -4,6 +4,6 @@ package jfs.service.transferobjects;
  * Created by lpuddu on 3-11-2015.
  */
 public class LoginDTO {
-    public String id;
+    public String email;
     public String password;
 }
