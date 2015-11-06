73e51e61066e97326cd8a22721e6f01f25f7875f
verbeen <l_puddu@hotmail.com>
2015-11-06 23:25:54 +0100

    tiny cleanup. backend should be fully functional now.

--- JFSService/src/main/java/jfs/service/services/UserWebService.java
+++ JFSService/src/main/java/jfs/service/services/UserWebService.java
@@ -3,10 +3,6 @@ package jfs.service.services;
 import com.google.gson.Gson;
 import jfs.service.transferobjects.LoginDTO;
 import jfs.service.transferobjects.RegisterDTO;
-//import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
-
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 
@@ -19,14 +15,12 @@ public class UserWebService
     @Inject
     UserService service;
 
-    //@CrossOriginResourceSharing(allowOrigins = { "*" })
     @POST @Path("/register") @Consumes("application/json") @Produces("application/json")
     public String register(RegisterDTO register){
         Boolean result = this.service.registerStudent(register.email, register.password);
         return new Gson().toJson(result);
     }
 
-    //@CrossOriginResourceSharing(allowOrigins = { "*" })
     @POST @Path("/login") @Consumes("application/json") @Produces("application/json")
     public String login(LoginDTO login){
         return new Gson().toJson(this.service.loginUser(login.email, login.password));
