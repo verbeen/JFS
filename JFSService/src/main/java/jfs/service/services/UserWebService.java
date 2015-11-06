b20b8641622547a1869addbb2b5a58a144904ae4
verbeen <l_puddu@hotmail.com>
2015-11-06 23:16:28 +0100

    checking if CROS was actually the issue since service.war wasnt even getting deployed (mongodb connection was the issue)

--- JFSService/src/main/java/jfs/service/services/UserWebService.java
+++ JFSService/src/main/java/jfs/service/services/UserWebService.java
@@ -3,7 +3,7 @@ package jfs.service.services;
 import com.google.gson.Gson;
 import jfs.service.transferobjects.LoginDTO;
 import jfs.service.transferobjects.RegisterDTO;
-import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
+//import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
 
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -19,14 +19,14 @@ public class UserWebService
     @Inject
     UserService service;
 
-    @CrossOriginResourceSharing(allowOrigins = { "*" })
+    //@CrossOriginResourceSharing(allowOrigins = { "*" })
     @POST @Path("/register") @Consumes("application/json") @Produces("application/json")
     public String register(RegisterDTO register){
         Boolean result = this.service.registerStudent(register.email, register.password);
         return new Gson().toJson(result);
     }
 
-    @CrossOriginResourceSharing(allowOrigins = { "*" })
+    //@CrossOriginResourceSharing(allowOrigins = { "*" })
     @POST @Path("/login") @Consumes("application/json") @Produces("application/json")
     public String login(LoginDTO login){
         return new Gson().toJson(this.service.loginUser(login.email, login.password));
