6f9fd7f40174390f14c61cf7916954f058535a83
toaditoad <communitiestk@me.com>
2015-11-15 12:39:00 +0100

    added parameter filters for administer job offers

--- JFSGui/src/main/webapp/app/administration/job/list/administration-job-list.js
+++ JFSGui/src/main/webapp/app/administration/job/list/administration-job-list.js
@@ -5,7 +5,6 @@
         .module('app')
         .controller('AdministrationJobListController', AdministrationJobListController);
 
-
     AdministrationJobListController.$inject = ['JobService', '$scope'];
     function AdministrationJobListController(JobService, $scope) {
 
@@ -17,9 +16,34 @@
             console.log("remove()");
         };
 
-        $scope.getTimes = function(n){
+        $scope.getTimes = function(n) {
             return new Array(n);
         };
+
+        $scope.quicksearch = function() {
+            console.log("quicksearch()");
+        };
+
+        $scope.jobFunction = [
+            { "value": "Developer", "label": "Developer" },
+            { "value": "Designer", "label": "Designer" },
+            { "value": "Project Manager", "label": "Project Manager" }
+        ];
+
+        $scope.jobDuration = [
+            { "value": "1month", "label": "One month" },
+            { "value": "2months", "label": "Two months" },
+            { "value": "3months", "label": "Three months" },
+            { "value": "6months", "label": "Six months" }
+        ];
+
+        $scope.jobLocation = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut",
+            "Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky",
+            "Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri",
+            "Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota",
+            "North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina",
+            "South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia",
+            "Wisconsin","Wyoming"];
 
         /*
         var amount = 5;
