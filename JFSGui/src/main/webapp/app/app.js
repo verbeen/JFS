9a4633887266dfb633ca98936a8777e9a52fefb0
toaditoad <communitiestk@me.com>
2015-11-16 20:36:52 +0100

    removed setting authorization header

--- JFSGui/src/main/webapp/app/app.js
+++ JFSGui/src/main/webapp/app/app.js
@@ -52,9 +52,6 @@
     function run($rootScope, $location, $cookieStore, $http) {
         // keep user logged in after page refresh
         $rootScope.globals = $cookieStore.get('globals') || {};
-        if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-        }
 
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
             // redirect to login page if not logged in and trying to access a restricted page
