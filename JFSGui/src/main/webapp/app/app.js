d0eadbb96081d0bbf04a2292d1dfc748eb97a36e
toaditoad <communitiestk@me.com>
2015-11-17 13:06:30 +0100

    added masonry for job offer ordering in job offer list view

--- JFSGui/src/main/webapp/app/app.js
+++ JFSGui/src/main/webapp/app/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('app', ["ngRoute", "ngCookies", "ngSanitize", "mgcrea.ngStrap"])
+    angular.module('app', ["ngRoute", "ngCookies", "ngSanitize", "mgcrea.ngStrap", "wu.masonry"])
         .config(config)
         .run(run)
         .controller('HomeController', HomeController)
