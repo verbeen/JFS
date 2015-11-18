a8fea0bc014d17fb8c652c230ece624014fc60a7
toaditoad <communitiestk@me.com>
2015-11-18 13:10:43 +0100

    - cleaned create job view - added validation for fields - added error messages - added reset function

--- JFSGui/src/main/webapp/app/app.js
+++ JFSGui/src/main/webapp/app/app.js
@@ -6,7 +6,9 @@
         .run(run)
         .controller('HomeController', HomeController)
         .filter('duration', DurationFilter)
-        .filter('htmlToPlainText', HtmlToPlainTextFilter);
+        .filter('htmlToPlainText', HtmlToPlainTextFilter)
+        .directive('showErrors', ShowErrorsDirective)
+        .directive('minLength', BsSelectValidationDirective);
 
     config.$inject = ['$routeProvider'];
     function config($routeProvider, $rootScope) {
@@ -96,6 +98,65 @@
     function HtmlToPlainTextFilter() {
         return function(text) {
             return  text ? String(text).replace(/<[^>]+>/gm, '') : '';
+        };
+    }
+
+    ShowErrorsDirective.$inject = ['$timeout'];
+    function ShowErrorsDirective($timeout) {
+        return {
+            restrict: 'A',
+            require: '^form',
+            link: function (scope, el, attrs, formCtrl) {
+                // find the text box element, which has the 'name' attribute
+                var inputEl = el[0].querySelector("[name]");
+                // convert the native text box element to an angular element
+                var inputNgEl = angular.element(inputEl);
+                // get the name on the text box so we know the property to check
+                // on the form controller
+                var inputName = inputNgEl.attr('name');
+
+                // only apply the has-error class after the user leaves the text box
+                inputNgEl.bind('blur', function () {
+                    el.toggleClass('has-error', formCtrl[inputName].$invalid);
+                })
+
+                scope.$on('show-errors-check-validity', function() {
+                    el.toggleClass('has-error', formCtrl[inputName].$invalid);
+                });
+
+                scope.$on('show-errors-reset', function() {
+                    $timeout(function() {
+                        el.removeClass('has-error');
+                    }, 0, false);
+                });
+            }
+        };
+    }
+
+    BsSelectValidationDirective.$inject = [];
+    function BsSelectValidationDirective() {
+        return {
+            require: 'ngModel',
+            link: function (scope, elem, attr, ngModel) {
+                var minLength = attr.minLength;
+
+                //For model -> DOM validation
+                ngModel.$formatters.unshift(function (value) {
+                    if (ngModel.$viewValue !== undefined) {
+                        var valid = (ngModel.$viewValue.length >= minLength);
+                        ngModel.$setValidity('minLength', valid);
+                        return value;
+                    }
+                });
+
+                scope.$watch(attr.ngModel, function (newVal) {
+                    if (angular.isDefined(newVal)) {
+                        var valid = (newVal.length >= minLength);
+                        ngModel.$setValidity('minLength', valid);
+                        return valid ? ngModel.$viewValue : undefined;
+                    }
+                }, true);
+            }
         };
     }
 })();
--- JFSGui/src/main/webapp/app/job/create/job-create.js
+++ JFSGui/src/main/webapp/app/job/create/job-create.js
@@ -5,20 +5,48 @@
         .module('app')
         .controller('JobCreateController', JobCreateController);
 
-
     JobCreateController.$inject = ['JobService', '$scope', '$rootScope'];
     function JobCreateController(JobService, $scope, $rootScope) {
         var vm = this;
 
+        vm.jobProfileParams = {
+            "type": [
+                { "value": "master_thesis", "label": "Master thesis" },
+                { "value": "bachelor_thesis", "label": "Bachelor thesis" },
+                { "value": "part_time", "label": "Part time" },
+                { "value": "full_time", "label": "Full time" },
+                { "value": "internship", "label": "Internship" },
+                { "value": "contract", "label": "Contract" }
+            ]
+        };
 
-
+        vm.initializeNewJobOffer = initializeNewJobOffer;
         vm.create = create;
+        vm.reset = reset;
+
+        // gets executed on initial load
+        (function initController() {
+            // reset job create view
+            vm.initializeNewJobOffer();
+        })();
+
+        function initializeNewJobOffer() {
+            vm.responseMessage = {};
+            vm.responseMessage.showForm = true;
+            vm.responseMessage.success = false;
+            vm.responseMessage.error = false;
+            vm.reset();
+        }
 
         function create() {
-            console.log(vm.jobProfile);
+            vm.dataLoading = true;
 
-            var a = Date.parse(vm.jobProfile.durationFrom);
-            console.log(a);
+            $scope.$broadcast('show-errors-check-validity');
+
+            if ($scope.formCreateJob.$invalid) {
+                vm.dataLoading = false;
+                return;
+            }
 
             var obj = {
                 "companyId": $rootScope.globals.currentUser.username,
@@ -42,20 +70,37 @@
 
             JobService.createJob(obj)
                 .then(function (response) {
+                    vm.responseMessage.showForm = false;
                     if (response.success) {
-                        console.log(response);
+                        console.info("Job offer created!");
+                        vm.responseMessage.success = true;
+                        vm.responseMessage.text = "Your job offer has been successfully created.";
+                        vm.dataLoading = false;
+                    } else {
+                        // backend service is not reachable (e.g. database down)
+                        console.error(response.message);
+                        vm.responseMessage.error = true;
+                        vm.responseMessage.text = "An error occurred while creating your job offer.";
+                        vm.dataLoading = false;
                     }
                 });
         }
-        vm.jobProfileParams = {
-            "type": [
-                { "value": "master_thesis", "label": "Master thesis" },
-                { "value": "bachelor_thesis", "label": "Bachelor thesis" },
-                { "value": "part_time", "label": "Part time" },
-                { "value": "full_time", "label": "Full time" },
-                { "value": "internship", "label": "Internship" },
-                { "value": "contract", "label": "Contract" }
-            ]
-        };
+
+        function reset() {
+            vm.jobProfile = {
+                "jobName": "",
+                "type": "",
+                "jobDescription": "",
+                "jobTask": "",
+                "validTilldate": "",
+                "durationFrom": "",
+                "durationTo": "",
+                "location": "",
+                "address": "",
+                "contactPerson": "",
+                "email": ""
+            };
+            $scope.$broadcast('show-errors-reset');
+        }
     }
 })();
