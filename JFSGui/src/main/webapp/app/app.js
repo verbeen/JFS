4396e2f3bc2e974ec36a753b4fbcff6c5e497dbf
DeepikaPrasad <52prde1mst@hft-stuttgart.de>
2015-12-06 17:52:44 +0100

    Displaying job offers on google map

--- JFSGui/src/main/webapp/app/app.js
+++ JFSGui/src/main/webapp/app/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('app', ["ngRoute", "ngCookies", "ngSanitize", "mgcrea.ngStrap", "wu.masonry"])
+    angular.module('app', ["ngRoute", "ngCookies", "ngSanitize", "mgcrea.ngStrap", "wu.masonry", "ngMap"])
         .config(config)
         .run(run)
         .controller('HomeController', HomeController)
