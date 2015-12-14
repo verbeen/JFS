b16435d40b38805e6bd85e973d51d2cf4989b934
verbeen <l_puddu@hotmail.com>
2015-12-14 15:46:56 +0100

    Partial search for location

--- JFSData/src/main/java/jfs/data/stores/JobOfferStore.java
+++ JFSData/src/main/java/jfs/data/stores/JobOfferStore.java
@@ -101,13 +101,12 @@ public class JobOfferStore extends DataStore {
             pairs.add(new Pair("type", studentSubscriptionsDO.type.name()));
         }
         if(studentSubscriptionsDO.location != null && !"".equals(studentSubscriptionsDO.location)){
-            pairs.add(new Pair("location", studentSubscriptionsDO.location));
+            pairs.add(new Pair("location", new BasicDBObject("$regex", studentSubscriptionsDO.location)));
         }
         if(studentSubscriptionsDO.skills != null && !"".equals(studentSubscriptionsDO.skills)){
             //List<String> Skills = Arrays.asList(studentSubscriptionsDO.skills.split("\\s*,\\s*")); //also deleted any additional white spaces
             String skillsRegex = studentSubscriptionsDO.skills.replace(",", "|");
             skillsRegex = skillsRegex.replace(" ", "");
-            System.out.println(skillsRegex);
 
             pairs.add(new Pair("skills", new BasicDBObject("$regex", skillsRegex)));
         }
